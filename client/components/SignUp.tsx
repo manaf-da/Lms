a363cc32578a2398f986b5e6dbc085d99efce866
manaf-da <manafadam@gmail.com>
2024-04-29 06:47:42 +0200

    frontend ui

---
+++ client/components/SignUp.tsx
@@ -0,0 +1,10 @@
+import React ,{FC} from 'react';
+
+type Props ={}
+
+
+const SignUp: FC<Props> = () => {
+
+}
+
+export default SignUp
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Montserrat, Open_Sans, Roboto } from "next/font/google"
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -8,16 +9,31 @@ const config = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
 	],
+ /*  darkMode:['class'], */
   prefix: "",
   theme: {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
     },
     extend: {
+      fontFamily:{
+        roboto:["var(--font-roboto)"],
+        montserrat:["var(--font-montserrat)"],
+      },
+      backgroundImage:{
+        "gradient-radial":"radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":"conic-gradient(from 180 deg at 50%  50%, var(--tw-gradient-stops)"
+      },
+      screens:{
+        "1000px": "1000px",
+        "1100px": "1100px",
+        "1200px": "1200px",
+        "1300px": "1300px",
+        "1500px": "1500px",
+        "800px": "800px",
+        "400px": "400px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
